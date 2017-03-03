6c29e54551bbd36c166017d73e969ffc531f235a
Ablay Keldibek <atomio.ak@gmail.com>
2017-03-03 06:51:18 +0600

    Update index.js

--- challenge/index.js
+++ challenge/index.js
@@ -11,7 +11,7 @@ window.__challenge__ = {
   ablay: {
     name: 'Ablay',
     items: [
-
+      'https://psv4.userapi.com/c812521/u267974059/docs/9d4b49604d49/day_1.jpg?extra=EiS8GvTXARHOlwkX4ESSd5YtKPvTqaeFhoXdHr5wpbcYC3cleWL_50TaeQyFZ4lZtCKgPlXi0qroLtm4tK5AW8HdBHF3djA5ILiEahXqPo--ieyduP-2l6rUjg',
     ],
   },
 };
