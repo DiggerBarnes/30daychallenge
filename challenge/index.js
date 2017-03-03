f985a1336fc96041d93c57da2637653cbdd8365c
Ablay Keldibek <atomio.ak@gmail.com>
2017-03-03 18:05:51 +0600

    Update index.js

--- challenge/index.js
+++ challenge/index.js
@@ -4,14 +4,14 @@ window.__challenge__ = {
   diyaz: {
     name: 'Diyaz',
     items: [
-      'https://cs7055.userapi.com/c812625/u382125570/docs/7027cedbd238/day_1.jpg?extra=HylLN9ncg_2N7SvOV0j3dGtjPmmLs6cJR_tjR8vHdXr_ffYVepebYnRct1lqx0F6hMTAylrS-jhI-nYO5LdcTtOxTMr7O4IWtqW2ZC5rfCEspKRazhpfRbXXAQ',
+      'http://i.imgur.com/CuMUPte.jpg',
     ],
   },
 
   ablay: {
     name: 'Ablay',
     items: [
-      'https://psv4.userapi.com/c812521/u267974059/docs/9d4b49604d49/day_1.jpg?extra=EiS8GvTXARHOlwkX4ESSd5YtKPvTqaeFhoXdHr5wpbcYC3cleWL_50TaeQyFZ4lZtCKgPlXi0qroLtm4tK5AW8HdBHF3djA5ILiEahXqPo--ieyduP-2l6rUjg',
+      'http://i.imgur.com/QAb0gBQ.jpg',
     ],
   },
 };
